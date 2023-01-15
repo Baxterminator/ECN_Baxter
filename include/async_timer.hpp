31a9bb77ba3b32bb9c5ee3b45d6208d0684834da
meltwin <meltwin158@gmail.com>
2023-01-15 21:33:35 +0100

    + Added pressure checker to auto release when there isn't enough suction

---
+++ include/async_timer.hpp
@@ -0,0 +1,39 @@
+//
+// Created by Geoffrey Côte on 14/01/23.
+//
+
+#ifndef ASYNC_TIMER_H
+#define ASYNC_TIMER_H
+
+#include <thread>
+#include <chrono>
+#include <functional>
+#include <iostream>
+
+using namespace std;
+using namespace chrono_literals;
+using namespace chrono;
+using namespace std::this_thread;
+
+class AsyncTimer {
+public:
+    AsyncTimer() = default;
+    AsyncTimer(function<void()> callable, milliseconds duration)
+    {
+        cout << "Init Timer" << endl;
+        runner = thread([](auto f, auto loop_time){
+            for (;;) {
+                cout << "Thread is running" << endl;
+                if (f == nullptr)
+                    break;
+                f();
+                sleep_for(loop_time);
+            }
+        }, callable, duration);
+    };
+private:
+    thread runner;
+};
+
+
+#endif //ASYNC_TIMER_H
