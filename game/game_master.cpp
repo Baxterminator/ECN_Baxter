78de2060b6cf65fd1cda1144c2c07dfbcc28a866
meltwin <meltwin158@gmail.com>
2023-02-06 20:08:05 +0100

    Switching to C++ QT5 GUI

--- game/game_master.cpp
+++ game/game_master.cpp
@@ -1,5 +1,7 @@
 #include <ecn_baxter/game/node.hpp>
 #include <csignal>
+#include <ui_main.h>
+#include "QtWebKit"
 
 using namespace ECNBaxter;
 
@@ -14,6 +16,16 @@ int main(int argc, char** argv) {
 
     signal(SIGINT, &sighandler);
 
+    Ui_BaxterMaster gui;
+
+    //QWebView::initialize();
+    QApplication app(argc, argv);
+    QMainWindow widget;
+
+
+    gui.setupUi(&widget);
+    widget.show();
+    app.exec();
 
     // Init the two nodes
     if (!Node::init(argc, argv))
--- gui/ecn_baxter/GUINode.py
+++
@@ -1,6 +0,0 @@
-from rclpy import Node
-
-
-class GUINode(Node):
-    def __init__(self):
-        super().__init__('game_gui')
