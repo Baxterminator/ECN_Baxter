c8d6688bb6d544e170f82fe3ac45050781fdff25
meltwin <meltwin158@gmail.com>
2023-02-09 09:43:18 +0100

    Setup client

--- game/node2.cpp
+++ game/node2.cpp
@@ -1,7 +1,84 @@
 #include <ecn_baxter/game/node2.hpp>
 
 namespace ECNBaxter {
+
+    GMROS2::GMROS2(rclcpp::NodeOptions opts) : rclcpp::Node("game_master_2", opts) {
+        ptn_setup = rclcpp_action::create_client<PointsSetup>(this, "points_setup");
+    }
+
+    void GMROS2::bind_gui() {
+        QObject::connect(UIWrapper::setup(), &QPushButton::clicked, [this]() {launch_setup();});
+    }
+
+    /* =================================================================
+     #                              ACTION
+     #                            Points setup
+     # =================================================================*/
+
     void GMROS2::launch_setup() {
         RCLCPP_INFO(get_logger(), "Setup launch !");
+
+        // Setup MSG
+        auto setup_msg = PointsSetup::Goal();
+        setup_msg.ptns_name = std::vector<std::string>{
+            "test1",
+            "test2",
+            "test3",
+            "test4"
+        };
+        setup_msg.sides = std::vector<bool>{
+          setup_msg.LEFT,
+          setup_msg.LEFT,
+          setup_msg.RIGHT,
+          setup_msg.RIGHT
+        };
+
+        auto goal_options = Client<PointsSetup>::SendGoalOptions();
+        goal_options.goal_response_callback = [&] (const PtnSetupHandler::SharedPtr &handle) {
+            ptn_setup_goal(handle);
+        };
+        goal_options.feedback_callback = [&] (PtnSetupHandler::SharedPtr handle, const sptr<const PointsSetup::Feedback> feedback) {
+            ptn_setup_feedback(handle, feedback);
+        };
+        goal_options.result_callback = [&] (const PtnSetupHandler::WrappedResult &result) {
+            ptn_setup_result(result);
+        };
+
+        ptn_setup->async_send_goal(setup_msg, goal_options);
+    }
+
+    void GMROS2::ptn_setup_goal(const PtnSetupHandler::SharedPtr &handle) {
+        if (!handle) {
+            RCLCPP_ERROR(get_logger(), "Setup rejected bu server");
+        }
+        RCLCPP_INFO(get_logger(), "Setup launched !");
+    }
+
+    void GMROS2::ptn_setup_result(const PtnSetupHandler::WrappedResult &result) {
+        switch (result.code) {
+            case ResultCode::ABORTED:
+                RCLCPP_ERROR(get_logger(), "Setup aborted !");
+                break;
+            case ResultCode::CANCELED:
+                RCLCPP_ERROR(get_logger(), "Setup canceled !");
+                break;
+            case ResultCode::UNKNOWN:
+                RCLCPP_ERROR(get_logger(), "Unknown result code :/");
+                break;
+        }
+        RCLCPP_INFO(get_logger(), "Setup done with result %d", result.result);
+    }
+
+    void GMROS2::ptn_setup_feedback(PtnSetupHandler::SharedPtr &handle,
+                                    const sptr<const PointsSetup::Feedback> feedback) 
+    {
+        auto p_name = feedback->ptn_name;
+        auto p = feedback->ptn;
+        RCLCPP_INFO(get_logger(),
+        "Receiving marker %s at (%f, %f, %f)",
+        p_name.c_str(),
+        p.x,
+        p.y,
+        p.z);
     }
 }
--- include/ecn_baxter/game/node2.hpp
+++ include/ecn_baxter/game/node2.hpp
@@ -2,20 +2,40 @@
 #define NODE2_HPP
 
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
 #include <ecn_baxter/game/gui_wrapper.hpp>
+#include <ecn_baxter/action/points_setup.hpp>
+#include <memory>
+#include <functional>
 
 namespace ECNBaxter {
+
+    using rclcpp_action::Client;
+    using rclcpp_action::ResultCode;
+    using ecn_baxter::action::PointsSetup;
+    using PtnSetupHandler = rclcpp_action::ClientGoalHandle<PointsSetup>;
+
+    template<typename F>
+    using sptr = std::shared_ptr<F>;
+
 /**
  * ROS 2 Node for the game master
 */
 class GMROS2 : public rclcpp::Node {
 public:
-    explicit GMROS2(rclcpp::NodeOptions opts) : rclcpp::Node("game_master_2", opts) {}
-    inline void bind_gui() {
-        QObject::connect(UIWrapper::setup(), &QPushButton::clicked, [this]() {launch_setup();});
-    }
+    explicit GMROS2(rclcpp::NodeOptions opts);
+    void bind_gui();
 protected:
+    /* =================================================================
+     #                              ACTION
+     #                            Points setup
+     # =================================================================*/
+    Client<PointsSetup>::SharedPtr ptn_setup;
     void launch_setup();
+    void ptn_setup_goal(const PtnSetupHandler::SharedPtr &handle);
+    void ptn_setup_feedback(PtnSetupHandler::SharedPtr &handle,
+                            const sptr<const PointsSetup::Feedback> feedback);
+    void ptn_setup_result(const PtnSetupHandler::WrappedResult &result);
 };
 
 }
